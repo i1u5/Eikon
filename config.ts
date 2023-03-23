0d478636974d37d7a8a2e6111ae23e324ca21fa5
Ilus <doomlordcs@gmail.com>
2023-03-23 20:07:35 +0000

    Change UA

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ const constants = {
     // CDN url !!without a trailing slash!! from >> https://github.com/i1u5/wrangler-cdn
     cdn: "cdn.medzik.xyz",
     // User agent for the requests, the default does the job
-    reqUA: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+    reqUA: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
     // Cookie to be used for requests, set it to 0 if you don't want to process NSFW galleries
     reqCookie: "over18=1",
     // Temporary render solution until more complicated operations are required
@@ -12,7 +12,7 @@ const constants = {
         footer:
             `<footer><small>Built with 💖 by <a href="https://github.com/i1u5">Ilµs</a><br>Inspired by 📷 <a href="https://git.voidnet.tech/kev/imgin">Imgin</a><br>This website does not claim ownership of any media.<br>This service simply acts as a proxy to Imgur.com and does not store any images.<br>Abusive images should be reported to imgur. This website does not create new images.<br>Do not use this service to facilitate scraping or as an image CDN.</small></footer></body></html>`,
     },
-    version: "0.0.1"
+    version: "0.0.2"
 };
 
 export default constants;
